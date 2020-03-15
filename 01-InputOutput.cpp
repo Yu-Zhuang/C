e9d5873e5c722f85445d6d780d242d1926a851d1
mild-guy <106711129@gms.tcu.edu.tw>
2020-03-15 14:16:59 +0800

    variable, input, output

---
+++ 01-InputOutput.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	//variable announce
+	int a=0, b=3; //interger, 4 byte, -2147483648 ~ +2147483648 (2^32);
+	float c=1.2; //float, 4byte, 1.2e-38 ~ 3.4e38;
+	double d=2.7 //double, 8 byte, 2.2e-308 ~ 1.8e308;
+	char e='a' //char, 1byte, 0~255;
+	char f[]="hello world" //string;
+
+	//input, output
+	printf("origin: %d %f %lf %c %s\n", a, c, d, e, f); //output : show origin variable value;
+	scanf("%d %f %lf %c %s", &a, &c, &d, &e, f); //input
+	printf("new: %d %f %lf %c %s", a, c, d, e, f); //output (after input new value);
+	
+	return 0;
+}
