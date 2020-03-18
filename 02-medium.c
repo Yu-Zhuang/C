8301be4cd596c46e6fe217f6395479799b229fc0
mild-guy <106711129@gms.tcu.edu.tw>
2020-03-18 18:47:52 +0800

    add medium level file

---
+++ 02-medium.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// returnType functionName (argumentType argument);
+int HiFunction(int N);
+
+int main(void){
+	int a=0, flag=1, N=0;
+
+//flow control project:
+	while(flag){ //while loop;
+		printf("enter a number between 1~9(end enter 0): ");
+		scanf("%d", &a);
+		// operator: >, <, ==, <=, >=, &&, ||
+		while(a < 0 || a>9) { printf("wrong input! input again: "); scanf("%d", &a); }
+		//flow control: if, else if, else
+		if(a < 5 && a > 0){
+			printf("a < 5 && a > 0\n");
+		}else if(a > 5){
+			printf("a > 5\n");
+		}else if(a == 5){
+			printf("a == 5\n");
+		}else{
+			flag=0;
+		}
+		N++;
+	}
+	//for loop
+	for(int i=0 ; i<N ; i++){
+		printf("you run %d times\n", i+1);
+	}
+
+//function project:
+	//function annouce (see line 4 and 5);
+	if(HiFunction(N)) printf("function run success"); //use function which defined by youself;
+
+	return 0;
+}
+//
+int HiFunction(int N){
+	printf("Hi , you run %d times\n",N);
+	return 1;
+}
