2a06c013617b52dc702e9e3eac3f483109399e69
mild-guy <106711129@gms.tcu.edu.tw>
2020-03-18 18:46:42 +0800

    modify basic file

--- 01-basic.c
+++ 01-basic.c
@@ -6,9 +6,9 @@ int main(void){
 	//variable announce
 	int a=0, b=3; //interger, 4 byte, -2147483648 ~ +2147483648 (2^32);
 	float c=1.2; //float, 4byte, 1.2e-38 ~ 3.4e38;
-	double d=2.7 //double, 8 byte, 2.2e-308 ~ 1.8e308;
-	char e='a' //char, 1byte, 0~255;
-	char f[]="hello world" //string;
+	double d=2.7; //double, 8 byte, 2.2e-308 ~ 1.8e308;
+	char e='a'; //char, 1byte, 0~255;
+	char f[]="hello world"; //string;
 
 	//input, output
 	printf("origin: %d %f %lf %c %s\n", a, c, d, e, f); //output : show origin variable value;
