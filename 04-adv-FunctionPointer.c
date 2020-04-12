33d02a5f1a305fb834d6764600981f223b0a3ce7
mild-guy <106711129@gms.tcu.edu.tw>
2020-04-12 11:28:02 +0800

    update

--- 04-adv-FunctionPointer.c
+++ 04-adv-FunctionPointer.c
@@ -1,3 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void add(int a,int b);
+
+int main(void){
+	void (*ad)(int a, int b) = &add;
+	
+	(*ad)(5,3);
+
+	return 0;
+}
+
+void add(int a,int b){ 
+	printf("%d\n", a+b); 
+}
+/*
 #include<stdio.h>  
 #include<stdlib.h>  
 #include<string.h>
@@ -32,4 +49,4 @@ void CHOSE(NODE *head, int chose){
 			break;
 	}
 }
-
+*/
